3395f20f3fc7298aa548a19c2077dfd602f48374
Jonathan Sommer <johnyso@web.de>
2014-12-03 10:51:31 +0100

    refactor newsController

--- classes/controllers/NewsController.php
+++ classes/controllers/NewsController.php
@@ -18,41 +18,14 @@ class NewsController extends AbstractController
     public function get($request)
     {
         $articles = $this->readArticles();
-        switch (count($request->elements)) {
+        $count = count($request->elements)-1;
+        switch (count($request->elements[$count])) {
             case 1:
                 return $articles;
             break;
             case 2:
-                $article_id = $request->url_elements[1];
+                $article_id = $request->elements[$count][1];
                 return $articles[$article_id];
-            break;
-        }
-    }
-    
-    /**
-     * POST action.
-     *
-     * @param  $request
-     * @return null
-     */
-    public function post($request)
-    {
-        switch (count($request->url_elements)) {
-            case 1:
-                // validation should go here
-                $id = (count($articles) + 1);
-                $articles = $this->readArticles();
-                $article = array(
-                    'id' => $id,
-                    'title' => $request->parameters['title'],
-                    'content' => $request->parameters['content'],
-                    'published' => date('c')
-                );
-                $articles[] = $article;
-                $this->writeArticles($articles);
-                header('HTTP/1.1 201 Created');
-                header('Location: /news/'.$id);
-                return null;
             break;
         }
     }
