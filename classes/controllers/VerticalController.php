0a5c291e15d4dba2ba98290b601bfed825eab6e0
Jonathan Sommer <johnyso@web.de>
2014-12-17 06:56:55 +0100

    add vertical controler

---
+++ classes/controllers/VerticalController.php
@@ -0,0 +1,36 @@
+<?php
+
+class VerticalController extends AbstractController
+{
+    /**
+     * GET method.
+     *
+     * @param  Request $request
+     * @return string
+     */
+    public function get($request)
+    {
+        $degree = $request->elements[0][1];
+        switch (count($request->elements[0])) {
+            case 1:
+                return "get horizontal alignment";
+                break;
+            case 2:
+                $request->executeStepper(1, $degree);
+
+                return "go to position ". $degree;
+                break;
+        }
+    }
+
+    /**
+     * PUT method.
+     *
+     * @param  Request $request
+     * @return string
+     */
+    public function put($request)
+    {
+        return "not implemented";
+    }
+}
