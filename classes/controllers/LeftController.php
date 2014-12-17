5e23879e2feaf0adc2db0b725fb063f4c2a3636e
Jonathan Sommer <johnyso@web.de>
2014-12-17 12:24:07 +0100

    add left right controller

---
+++ classes/controllers/LeftController.php
@@ -0,0 +1,36 @@
+<?php
+
+class LeftController extends AbstractController
+{
+    /**
+     * GET method.
+     *
+     * @param  Request $request
+     * @return string
+     */
+    public function get($request)
+    {
+        $degree = $request->elements[0][1];
+        switch (count($request->elements[0])) {
+            case 1:
+                return "not implemented";
+                break;
+            case 2:
+                $request->executeStepper(1, $degree);
+
+                return "go to position ". $degree;
+                break;
+        }
+    }
+
+    /**
+     * PUT method.
+     *
+     * @param  Request $request
+     * @return string
+     */
+    public function put($request)
+    {
+        return "not implemented";
+    }
+}
