0a43f0bad032e35b3e036a161c5752a5ea04c3fc
Jonathan Sommer <johnyso@web.de>
2014-12-17 12:18:11 +0100

    change to dev

--- classes/Request.php
+++ classes/Request.php
@@ -37,10 +37,6 @@ class Request
 
     public static function executeStepper($direction, $degree){
         // make execution
-        if(!$_SERVER['REMOTE_ADDR'] = "http://restful.local"){
             shell_exec("nodeStepper 5000000 " . $degree . " " . $direction ." 42 41 44 43");
-        } else {
-            echo ("nodeStepper 5000000 " . $degree . " " . $direction ." 42 41 44 43");
-        }
     }
 }
